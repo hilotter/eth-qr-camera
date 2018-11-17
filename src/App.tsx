92267c9f1a7ba2789df61a696dcdf7ece25b5056
hilotter <hilotter3@gmail.com>
2018-11-17 14:04:45 +0900

    add erc721 & google home link sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import axios from 'axios'
 import * as React from 'react'
 import QrReader from 'react-qr-reader'
+import erc721ABI from './erc721ABI'
 import web3 from './web3'
 
-const MIN_ETH = '0.0001'
+const GAME_CONTRACT_ADDRESS = '0x06012c8cf97bead5deae237070f9587f8e7a266d' // Crypto Kitties
+const GOOGLE_HOME_API_URL = 'https://d64a982d.ngrok.io/google-home-notifier'
 
 interface IState {
   address: string,
   balance: number,
   delay: number,
-  facingMode: "user" | "environment" | undefined,
-  success: boolean,
+  facingMode: "user" | "environment" | undefined
 }
 
 class App extends React.Component<any, IState> {
@@ -18,9 +20,8 @@ class App extends React.Component<any, IState> {
     this.state = {
       address: '',
       balance: 0,
-      delay: 3000,
-      facingMode: 'user',
-      success: false,
+      delay: 7000,
+      facingMode: 'user'
     }
 
     this.handleScan = this.handleScan.bind(this)
@@ -32,18 +33,25 @@ class App extends React.Component<any, IState> {
   }
 
   public getAddress = async (result) => {
-    this.setState({ address: '', balance: 0, success: false })
+    this.setState({ address: '', balance: 0 })
 
     const matches = result.match(/(0x[a-zA-Z0-9]+)/)
     if (matches) {
       const address = matches[1]
-      const ethBalance = await web3.eth.getBalance(address)
-      const balance = web3.utils.fromWei(ethBalance, 'ether')
-      const condition = web3.utils.toBN(web3.utils.toWei(MIN_ETH, 'ether'))
-      if (web3.utils.toBN(ethBalance).gte(condition)) {
-        this.setState({ success: true })
+      const contract = new web3.eth.Contract(erc721ABI, GAME_CONTRACT_ADDRESS);
+      const tokenBalance = await contract.methods.balanceOf(address).call()
+      const condition = web3.utils.toBN('1')
+
+      const params = new URLSearchParams();
+      if (web3.utils.toBN(tokenBalance).gte(condition)) {
+        params.append('text', "正解！次のヒントは「ゾンビ」です。");
+      } else {
+        params.append('text', "残念。猫を持っていないようです。");
       }
-      this.setState({ address, balance })
+      await axios.post(GOOGLE_HOME_API_URL, params)
+
+      console.log('ok')
+      this.setState({ address, balance: tokenBalance })
     }
   }
 
@@ -54,13 +62,14 @@ class App extends React.Component<any, IState> {
   public render(){
     const previewStyle = {
       height: 320,
-      marginLeft: "auto",
-      marginRight: "auto",
+      marginLeft: 'auto',
+      marginRight: 'auto',
       width: 320,
     }
 
     return(
-      <div>
+      <div style={{ textAlign: 'center' }}>
+        <p>Q1: 猫を示せ [<a href="https://www.cryptokitties.co/" target="_blank">Hint</a>]</p>
         <QrReader
           delay={this.state.delay}
           facingMode={this.state.facingMode}
@@ -68,8 +77,6 @@ class App extends React.Component<any, IState> {
           onError={this.handleError}
           onScan={this.handleScan}
         />
-        <p>Balance: {this.state.balance}ETH</p>
-        <p>Hold {MIN_ETH}ETH?: {this.state.success.toString()}</p>
       </div>
     )
   }
---
+++ src/erc721ABI.ts
@@ -0,0 +1,9 @@
+const erc721ABI = [{
+    "constant": true,
+    "inputs": [{ "name": "_owner", "type": "address" }],
+    "name": "balanceOf",
+    "outputs": [{ "name": "balance", "type": "uint256" }],
+    "type": "function"
+}];
+
+export default erc721ABI;
